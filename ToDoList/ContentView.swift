cbc1e839673b205c3f4c8f0064f33029c7e84fe3
Xinyi <Mingtianc8@gmail.com>
2025-08-08 13:11:42 -0700

    Added link from NewToDoView into contentView, wiht animation

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,25 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+   
+    @State private var showNewTask = false
+
     var body: some View {
+                
         VStack {
             HStack {
+                
                 Text("To-Do List")
                     .font(.title)
                     .fontWeight(.black)
+                
                 Spacer()
                 
                 Button {
-
+                   
+                    withAnimation {
+                        showNewTask = true
+                    }
+                    
                 } label: {
+                    
                     Text("+")
                         .font(.title)
                         .fontWeight(.bold)
+                    
                 }
                 
             }//HStack end
             .padding()
         }//Vstack end
+        if showNewTask == true {
+            NewToDoView()
+        }
     }//body end
 }//contentview end
 
